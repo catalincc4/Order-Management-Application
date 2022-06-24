ccca287b8c4fb0899504cb5d1b57e7fcb003a5e2
Catalin Calin <calincatalin99@gmail.com>
2022-06-24 20:38:33 +0300

    final

--- src/main/java/org/example/Connection/ConnectionFactory.java
+++ src/main/java/org/example/Connection/ConnectionFactory.java
@@ -8,8 +8,8 @@ public class ConnectionFactory {
     private static final Logger LOGGER = Logger.getLogger(ConnectionFactory.class.getName());
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String DBURL = "jdbc:mysql://localhost:3306/TP";
-    private static final String USER = "root";
-    private static final String PASS = "1234";
+    private static final String USER = "****";
+    private static final String PASS = "****";
 
     private static ConnectionFactory singleInstance = new ConnectionFactory();
 
